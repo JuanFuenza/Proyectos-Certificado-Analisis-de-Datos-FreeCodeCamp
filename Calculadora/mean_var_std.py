b45f5b377e7a2412f540d266a78a763d5bcd5cf3
Juan Fuenzalida <juan.fuenza97@gmail.com>
2022-06-30 13:41:58 -0300

    Calculo de varianza por fila, columna y elemento

--- Calculadora/mean_var_std.py
+++ Calculadora/mean_var_std.py
@@ -1,7 +1,7 @@
 """Crea una función llamada calculate() en mean_var_std.py que usa Numpy para producir la media, varianza, desviación estándar, max, min, y suma de las filas, columnas y elementos en una matriz de 3 x 3."""
 import numpy as np
 
-def calculate(list):
+def calculate(list: list):
     """Recibe una lista de 9 digitos.
     Convierte la lista en una matriz de 3x3 y luego 
     devuelve un diccionario con la media, varianza,
@@ -17,9 +17,15 @@ def calculate(list):
     'min': [[0, 1, 2], [0, 3, 6], 0],
     'sum': [[9, 12, 15], [3, 12, 21], 36]
     }"""
+    # Conversion de lista a array y a una matriz de 3 x 3.
     array = np.array(list).reshape((3,3))
+    # Calculo de la media por fila, por columna y elementos.
     mean = [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array)]
+    # Calculo de varianza por fila, columna y elementos.
+    variance = [np.var(array, axis=0).tolist(), np.var(array, axis=1).tolist(), np.var(array)]
     print(array)
     print(mean)
+    print(variance)
+    
     
 calculate([0,1,2,3,4,5,6,7,8])
