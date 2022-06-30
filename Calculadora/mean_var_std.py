c127f0ef52d7bc4340e3f3e2462ab23f5bc08271
Juan Fuenzalida <juan.fuenza97@gmail.com>
2022-06-30 16:17:31 -0300

    agregado calculo desviación estándar

--- Calculadora/mean_var_std.py
+++ Calculadora/mean_var_std.py
@@ -6,26 +6,19 @@ def calculate(list: list):
     Convierte la lista en una matriz de 3x3 y luego 
     devuelve un diccionario con la media, varianza,
     desviación estándar, max, min, suma a lo largo de
-    ambos ejes y para la matriz aplanada.
-    
-    >>> calculate([0,1,2,3,4,5,6,7,8])
-    {
-    'mean': [[3.0, 4.0, 5.0], [1.0, 4.0, 7.0], 4.0],
-    'variance': [[6.0, 6.0, 6.0], [0.6666666666666666, 0.6666666666666666, 0.6666666666666666], 6.666666666666667],
-    'standard deviation': [[2.449489742783178, 2.449489742783178, 2.449489742783178], [0.816496580927726, 0.816496580927726, 0.816496580927726], 2.581988897471611],
-    'max': [[6, 7, 8], [2, 5, 8], 8],
-    'min': [[0, 1, 2], [0, 3, 6], 0],
-    'sum': [[9, 12, 15], [3, 12, 21], 36]
-    }"""
+    ambos ejes y para la matriz aplanada."""
+
     # Conversion de lista a array y a una matriz de 3 x 3.
     array = np.array(list).reshape((3,3))
     # Calculo de la media por fila, por columna y elementos.
     mean = [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array)]
     # Calculo de varianza por fila, columna y elementos.
     variance = [np.var(array, axis=0).tolist(), np.var(array, axis=1).tolist(), np.var(array)]
+    # Calculo desviación estándar.
+    standard_deviation = [np.std(array, axis=0).tolist(), np.std(array, axis=1).tolist(), np.std(array)]
     print(array)
     print(mean)
     print(variance)
-    
+    print(standard_deviation)
     
 calculate([0,1,2,3,4,5,6,7,8])
